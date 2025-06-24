09ceda1a09e98649fc6c545d9f2d7388a8807bc9
qaasimahmad <40824771+qaasimahmad@users.noreply.github.com>
2025-06-24 04:02:04 +0100

    Delete week-3.1-js-algorithms directory

--- week-3.1-js-algorithms/checkSeason.js
+++
@@ -1,6 +0,0 @@
-const constants = require('../constants');
-function checkSeason(season){
-    const seasonNumberic = Number(season);
-    if(seasonNumberic <= 0 || seasonNumberic > 12 ) return "Inavalid season. Expected a value between [1, 12]";
-    return constants.seasons(season);
-}
--- week-3.1-js-algorithms/truncateString.js
+++
@@ -1,6 +0,0 @@
-function truncateString(word, maxLen){
-    if(!word || !maxLen) return 'Params cannot be empty. 2 params found 0'
-    if(typeof word !== 'string') return 'input must be of type string'
-    if(word.length > maxLen) return word.slice(0, maxLen -3)+ '...'
-    if(word.length <= maxLen -3) return word.replace(word, '...');
-}
