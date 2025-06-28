93617244beb24c177211cece79ceff39068744a6
qaasimahmad <40824771+qaasimahmad@users.noreply.github.com>
2025-06-28 18:05:55 +0100

    Delete week-4.1-js-algorithms directory

--- week-4.1-js-algorithms/task10.js
+++
@@ -1,8 +0,0 @@
-function palindrome(word){
-    if(!word) return "expected a param. Found none";
-
-    const wordArray = word.split("");
-    const reversedWord = wordArray.reverse().join("");
-
-    return word === reversedWord;
-}
