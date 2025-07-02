548a78455086cd142d543d9d29a26fe98f231068
qaasimahmad <40824771+qaasimahmad@users.noreply.github.com>
2025-07-02 06:00:27 +0100

    Code Refactor-logic fixes

---
+++ week-4.2-js-algorithms/challenge3.js
@@ -0,0 +1,29 @@
+// Challenge 3: Flatten a Two-Dimensional Array
+// Write a function that takes a two-dimensional array and returns a flattened array.
+
+function flattenArray(randomList){
+    if(!Array.isArray(randomList)){
+        return {
+            success: false,
+            message: "Invalid Input! list must be an array"
+        }
+    }
+
+    let flattenedArray = [];
+
+    for(let item of randomList){
+        if(Array.isArray(item)){
+            const flatItem = flattenArray(item);
+            flattenedArray.push(...flatItem);
+        } else{
+            flattenedArray.push(item);
+        }
+    }
+
+    return flattenedArray;
+}
+
+// Usage examples:
+// console.log(flattenArray([[1, 2], [3, 4], [5]])); // [1, 2, 3, 4, 5]
+// console.log(flattenArray([["a", "b"], ["c", "d"]])); // ["a", "b", "c", "d"]
+// console.log(flattenArray([[1], [2], [3]])); // [1, 2, 3]
---
+++ week-4.2-js-algorithms/challenge9.js
@@ -0,0 +1,21 @@
+// Challenge 9: Find the Intersection of Two Arrays
+// Write a function that takes two arrays and returns an array that is the intersection of the two arrays.
+
+function findIntersection(randomList1, randomList2){
+    if(!Array.isArray(randomList1) || !Array.isArray(randomList2)){
+        return {
+            success: false,
+            message: "Invalid Input! list must be an array"
+        }
+    }
+
+    const randomSet1 = new Set([...randomList1]);
+    const randomSet2 = new Set([...randomList2]);
+    const intersect = randomSet1.intersection(randomSet2);
+    return [...intersect];
+}
+
+// Usage examples:
+//console.log(findIntersection([1, 2, 3], [3, 4, 5])); // [3]
+//console.log(findIntersection(["a", "b", "c"], ["b", "c", "d"])); // ["b", "c"]
+//console.log(findIntersection([10, 20, 30], [40, 50, 60])); // []
