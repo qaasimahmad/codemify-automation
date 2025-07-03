640ae507f575c833984531220479a266a621dc17
qaasimahmad <40824771+qaasimahmad@users.noreply.github.com>
2025-07-03 04:39:24 +0100

    week-5-homework-submission

---
+++ week-5-JS-chellenges/helper.methods.js
@@ -0,0 +1,35 @@
+class Helpers {
+    getUsersByAge(response, ageMin, ageMax) {
+        const users = response.users
+            .filter((user)=> (user.age >= ageMin && user.age <= ageMax))
+            .map(user => user.name);
+
+        return users;
+    }
+
+    getUsersByRole(response, role) {
+        const users = response.users
+            .filter((user)=> user.role === role)
+            .map(user => user.name);
+
+        return users;
+    }
+
+    getUsersByCountry(response, country) {
+        const users = response.users
+            .filter((user)=> user.country === country)
+            .map(user => user.name);
+
+        return users;
+    }
+
+    getUsersByBalance(response, minBalance, maxBalance) {
+        const users = response.users
+            .filter((user)=> (user.balance > minBalance && user.balance < maxBalance))
+            .map(user => user.name);
+
+        return users;
+    }
+}
+
+export default new Helpers()
