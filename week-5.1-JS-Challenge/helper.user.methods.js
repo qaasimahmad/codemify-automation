3455ff144351108d171a312ec9ad725234f6bf2e
qaasimahmad <40824771+qaasimahmad@users.noreply.github.com>
2025-07-04 17:54:44 +0100

    Add files via upload
    Week-5-homework-submission

---
+++ week-5.1-JS-Challenge/helper.user.methods.js
@@ -0,0 +1,39 @@
+class Helpers {
+    getUsersByStatus(response, status) {
+        const users = response.users
+            .filter((user)=> (user.role.status === status && user.role.description))
+            .map(user => user.name)
+            
+        return users;
+    }
+
+    getUsersByRole(response, role, roleId) {
+        const users = response.users
+            .filter((user)=> (user.role.name === role && user.role.id === roleId))
+            .map(user => user.name)
+
+        return users;
+    }
+
+    getUsersByEmailDomain(response, domain) {
+        const users = response.users
+            .filter((user)=> (user.email.includes(domain)))
+            .map(user => user.name)
+
+        return users;
+    }
+
+    getUserBalanceByCurrency(response, currency) {
+        const balances = response.users
+            .filter((user)=> (user.currency === currency))
+            .map((user)=>{
+                const sanitizedBalance = Number(user.balance.replace(/[^0-9]/g, ''));
+                return sanitizedBalance;
+            })
+
+        return balances;
+    }
+
+}
+
+export default new Helpers()
