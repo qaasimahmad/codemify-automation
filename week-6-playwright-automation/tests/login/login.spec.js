986fdbec1af6e7e2b2dce8aeaa9b1a7decc13517
qaasimahmad <abumaahir1@gmail.com>
2025-07-10 09:53:56 +0100

    Added assertion for the user logged in

--- week-6-playwright-automation/tests/login/login.spec.js
+++ week-6-playwright-automation/tests/login/login.spec.js
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LoginPage } from "../pages/loginPage";
 import config from "../../playwright.config";
 
@@ -13,8 +13,15 @@ test.describe('Login and Logout', ()=>{
   test('Should log in with your existing account', async({page})=>{
     const loginPage = new LoginPage(page);
 
+    const firstName = "Qaasim";
+    const lastName = "Ahmad";
+    const expectedProfileName = `${firstName} ${lastName}`;
+
     await loginPage.logIn(page);
     await loginPage.assertProfilePageUrl(baseUrl);
+    const heading = page.getByRole('heading', { name: expectedProfileName });
+
+    await expect(heading).toHaveText(expectedProfileName);
   })
 
   test('Should log out', async({page})=>{
