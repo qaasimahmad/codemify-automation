a240a08b60119f2046c1ec32971611313109f7da
qaasimahmad <abumaahir1@gmail.com>
2025-07-12 16:24:19 +0100

    Stringified result to print out error object

--- Week-6.1-Automation-Extra-code-challenges/challenge3.js
+++ Week-6.1-Automation-Extra-code-challenges/challenge3.js
@@ -1,8 +1,15 @@
-/* eslint-disable no-unused-vars */
+
 // Challenge 3: Count positive and negative numbers in an array
 // Write a function to count positive and negative numbers in an array
 
 function countPositivesAndNegatives(numberList, type){
+  const typeEnum = [ 'positive', 'negative' ];
+  if(!typeEnum.includes(String(type))){
+    return {
+      success: false,
+      message: `type can only be ${typeEnum}`
+    }
+  }
   if(!Array.isArray(numberList)){
     return {
       success: false,
@@ -16,13 +23,6 @@ function countPositivesAndNegatives(numberList, type){
       message: "Type Error! All items in the array must be valid numbers."
     };
   }
-  const typeEnum = [ 'positive', 'negative' ];
-  if(!typeEnum.includes(String(type))){
-    return {
-      success: false,
-      message: `type can only be ${typeEnum}`
-    }
-  }
 
   const positives = numberList.filter((number)=> number > 0);
   const negatives = numberList.filter((number)=> number < 0);
@@ -31,6 +31,7 @@ function countPositivesAndNegatives(numberList, type){
 
 }
 
-//let arr = [10,20, -1,22,99,20, -9];
-// console.log(`The negative numbers in an array is ${countPositivesAndNegatives(arr, 'negative')}`) //"The negative numbers in an array is 2"
-// console.log(`The positive numbers in an array is ${countPositivesAndNegatives(arr, 'positive')}`) // "The positive numbers in an array is 5"
+let arr = [ 10,20, -1,22,99,20, -9 ];
+
+console.log(`The negative numbers in an array is ${JSON.stringify(countPositivesAndNegatives(arr, 'negative'))}`) //"The negative numbers in an array is 2"
+console.log(`The positive numbers in an array is ${JSON.stringify(countPositivesAndNegatives(arr, 'positive'))}`) // "The positive numbers in an array is 5"
